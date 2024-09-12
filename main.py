eb0c94bad6b3726d22f9285e682c733422313f00
Arnav Gupta <arnavgupta9000@gmail.com>
2024-09-12 11:17:42 -0600

    tested first set of queries on first batch

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class Query:
                                 genre TEXT NOT NULL,
                                 author_id INTEGER,
                                 publisher_id INTEGER,
+                                price INT DEFAULT 0, 
                                 FOREIGN KEY(author_id) REFERENCES authors(author_id),
                                 FOREIGN KEY(publisher_id) REFERENCES publishers(publisher_id))''')
         self.conn.commit()
@@ -54,29 +55,79 @@ class Query:
         ])
 
         # Insert sample data into books table
-        self.cursor.executemany('''INSERT INTO books (title, genre, author_id, publisher_id)
-                                   VALUES (?, ?, ?, ?)''', [
-            ('1984', 'Dystopian', 1, 1),
-            ('Harry Potter', 'Fantasy', 2, 2),
-            ('The Shining', 'Horror', 3, 3),
-            ('Animal Farm', 'Political Satire', 1, 1)
+        self.cursor.executemany('''INSERT INTO books (title, genre, author_id, publisher_id, price)
+                                   VALUES (?, ?, ?, ?, ?)''', [
+            ('1984', 'Dystopian', 1, 1, 500),
+            ('Harry Potter', 'Fantasy', 2, 2, 1000),
+            ('The Shining', 'Horror', 3, 3, 1),
+            ('Animal Farm', 'Political Satire', 1, 1, 10000)
         ])
         self.conn.commit()
 
     def close(self):
         self.cursor.close()
         self.conn.close()
+    
+    def search(self, query,values):
+
+        return self.cursor.execute(query, values).fetchall()
+
 
 
 # Usage
 query = Query()
-query.drop()  # Drop existing tables
-query.create_tables()  # Create the new tables
+# query.drop()  # Drop existing tables
+# query.create_tables()  # Create the new tables
 query.insert_data()  # Insert sample data
 
 
+###
+# get books < 1000, and publishers name
+# print(query.cursor.execute('''select distinct b.title 
+#                            from books b join publishers p on b.publisher_id = p.publisher_id
+#                            where b.price <1000;
+#                            ''', ()).fetchall())
+
+###
+
+###
 # get all books and their authors
-print(query.cursor.execute('''select ''', () ).fetchall())
+
+#dont use this method as implict joins are no where near as good as explict joins
+# print(query.cursor.execute('''select b.title, a.first_name, a.last_name
+#                            from books b, authors a
+#                            where b.author_id = a.author_id
+                           
+#                            ''', () ).fetchall())
+
+# print(query.cursor.execute('''select b.title, a.first_name, a.last_name
+#                            from books b join authors a on b.author_id = a.author_id;
+                           
+#                            ''', () ).fetchall())
+
+
+### 
+# get books along with their publishers
+
+# print(query.cursor.execute('''select b.title, p.name 
+#                            from books b join publishers p on b.publisher_id = p.publisher_id
+#                             ''', ()) .fetchall())
+
+
+### 
+# find all books in the genre Dystopian
+# print(query.cursor.execute('''select b.title 
+#                            from books b
+#                            where b.genre like lower('dystopian');
+#                            ''', ()).fetchall())
+
+
+###
+#Count the number of books by each author:
+# print(query.cursor.execute('''select a.first_name, a.last_name, count(b.book_id) as book_count
+#                            from books b join authors a on b.author_id = a.author_id
+#                            group by b.author_id;
+#                            ''', ()).fetchall())
 
 
 query.close()  # Close the connection
